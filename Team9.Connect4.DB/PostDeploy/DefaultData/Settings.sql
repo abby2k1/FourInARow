b0804577ed7ec432006073bf8ff958ed0c81f2ce
Abigail Thompson <abigail.thompson2001@outlook.com>
2023-04-07 20:09:01 -0500

    Settings Default Data

--- Team9.Connect4.DB/PostDeploy/DefaultData/Settings.sql
+++ Team9.Connect4.DB/PostDeploy/DefaultData/Settings.sql
@@ -1,1 +1,7 @@
-﻿
+﻿BEGIN
+	INSERT INTO dbo.tblSetting (Id, PlayerColor, OpponentColor, BoardColor)
+	VALUES
+	(NEWID(), 'Blue', 'Red', 'Black'),
+	(NEWID(), 'Green', 'Orange', 'White'),
+	(NEWID(), 'Purple', 'Green', 'Grey')
+END
