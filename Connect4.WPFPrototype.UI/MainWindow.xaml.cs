9ba63aba7c04a6f078907a96c32528b8f721fd92
Max <maxwell.wilke@yahoo.com>
2023-04-18 16:16:11 -0500

    Added Prototype WPF environment

---
+++ Connect4.WPFPrototype.UI/MainWindow.xaml.cs
@@ -0,0 +1,448 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Connect4.WPFPrototype.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnX44_Click(object sender, RoutedEventArgs e)
+        {
+            rec44.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO44_Click(object sender, RoutedEventArgs e)
+        {
+            rec44.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX43_Click(object sender, RoutedEventArgs e)
+        {
+            rec43.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO43_Click(object sender, RoutedEventArgs e)
+        {
+            rec43.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX42_Click(object sender, RoutedEventArgs e)
+        {
+            rec42.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO42_Click(object sender, RoutedEventArgs e)
+        {
+            rec42.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX41_Click(object sender, RoutedEventArgs e)
+        {
+            rec41.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO41_Click(object sender, RoutedEventArgs e)
+        {
+            rec41.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX40_Click(object sender, RoutedEventArgs e)
+        {
+            rec40.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO40_Click(object sender, RoutedEventArgs e)
+        {
+            rec40.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX39_Click(object sender, RoutedEventArgs e)
+        {
+            rec39.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO39_Click(object sender, RoutedEventArgs e)
+        {
+            rec39.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX56_Click(object sender, RoutedEventArgs e)
+        {
+            rec56.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO56_Click(object sender, RoutedEventArgs e)
+        {
+            rec56.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX55_Click(object sender, RoutedEventArgs e)
+        {
+            rec55.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO55_Click(object sender, RoutedEventArgs e)
+        {
+            rec55.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX54_Click(object sender, RoutedEventArgs e)
+        {
+            rec54.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO54_Click(object sender, RoutedEventArgs e)
+        {
+            rec54.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX53_Click(object sender, RoutedEventArgs e)
+        {
+            rec53.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO53_Click(object sender, RoutedEventArgs e)
+        {
+            rec53.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX52_Click(object sender, RoutedEventArgs e)
+        {
+            rec52.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO52_Click(object sender, RoutedEventArgs e)
+        {
+            rec52.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX51_Click(object sender, RoutedEventArgs e)
+        {
+            rec51.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO51_Click(object sender, RoutedEventArgs e)
+        {
+            rec51.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX68_Click(object sender, RoutedEventArgs e)
+        {
+            rec68.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO68_Click(object sender, RoutedEventArgs e)
+        {
+            rec68.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX67_Click(object sender, RoutedEventArgs e)
+        {
+            rec67.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO67_Click(object sender, RoutedEventArgs e)
+        {
+            rec67.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX66_Click(object sender, RoutedEventArgs e)
+        {
+            rec66.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO66_Click(object sender, RoutedEventArgs e)
+        {
+            rec66.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX65_Click(object sender, RoutedEventArgs e)
+        {
+            rec65.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO65_Click(object sender, RoutedEventArgs e)
+        {
+            rec65.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX64_Click(object sender, RoutedEventArgs e)
+        {
+            rec64.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO64_Click(object sender, RoutedEventArgs e)
+        {
+            rec64.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX63_Click(object sender, RoutedEventArgs e)
+        {
+            rec63.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO63_Click(object sender, RoutedEventArgs e)
+        {
+            rec63.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX80_Click(object sender, RoutedEventArgs e)
+        {
+            rec80.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO80_Click(object sender, RoutedEventArgs e)
+        {
+            rec80.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX79_Click(object sender, RoutedEventArgs e)
+        {
+            rec79.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO79_Click(object sender, RoutedEventArgs e)
+        {
+            rec79.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX78_Click(object sender, RoutedEventArgs e)
+        {
+            rec78.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO78_Click(object sender, RoutedEventArgs e)
+        {
+            rec78.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX77_Click(object sender, RoutedEventArgs e)
+        {
+            rec77.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO77_Click(object sender, RoutedEventArgs e)
+        {
+            rec77.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX76_Click(object sender, RoutedEventArgs e)
+        {
+            rec76.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO76_Click(object sender, RoutedEventArgs e)
+        {
+            rec76.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX75_Click(object sender, RoutedEventArgs e)
+        {
+            rec75.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO75_Click(object sender, RoutedEventArgs e)
+        {
+            rec75.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX92_Click(object sender, RoutedEventArgs e)
+        {
+            rec92.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO92_Click(object sender, RoutedEventArgs e)
+        {
+            rec92.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX91_Click(object sender, RoutedEventArgs e)
+        {
+            rec91.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO91_Click(object sender, RoutedEventArgs e)
+        {
+            rec91.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX90_Click(object sender, RoutedEventArgs e)
+        {
+            rec90.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO90_Click(object sender, RoutedEventArgs e)
+        {
+            rec90.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX89_Click(object sender, RoutedEventArgs e)
+        {
+            rec89.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO89_Click(object sender, RoutedEventArgs e)
+        {
+            rec89.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX88_Click(object sender, RoutedEventArgs e)
+        {
+            rec88.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO88_Click(object sender, RoutedEventArgs e)
+        {
+            rec88.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX87_Click(object sender, RoutedEventArgs e)
+        {
+            rec87.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO87_Click(object sender, RoutedEventArgs e)
+        {
+            rec87.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX104_Click(object sender, RoutedEventArgs e)
+        {
+            rec104.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO104_Click(object sender, RoutedEventArgs e)
+        {
+            rec104.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX103_Click(object sender, RoutedEventArgs e)
+        {
+            rec103.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO103_Click(object sender, RoutedEventArgs e)
+        {
+            rec103.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX102_Click(object sender, RoutedEventArgs e)
+        {
+            rec102.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO102_Click(object sender, RoutedEventArgs e)
+        {
+            rec102.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX101_Click(object sender, RoutedEventArgs e)
+        {
+            rec101.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO101_Click(object sender, RoutedEventArgs e)
+        {
+            rec101.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX100_Click(object sender, RoutedEventArgs e)
+        {
+            rec100.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO100_Click(object sender, RoutedEventArgs e)
+        {
+            rec100.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX99_Click(object sender, RoutedEventArgs e)
+        {
+            rec99.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO99_Click(object sender, RoutedEventArgs e)
+        {
+            rec99.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX116_Click(object sender, RoutedEventArgs e)
+        {
+            rec116.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO116_Click(object sender, RoutedEventArgs e)
+        {
+            rec116.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX115_Click(object sender, RoutedEventArgs e)
+        {
+            rec115.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO115_Click(object sender, RoutedEventArgs e)
+        {
+            rec115.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX114_Click(object sender, RoutedEventArgs e)
+        {
+            rec114.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO114_Click(object sender, RoutedEventArgs e)
+        {
+            rec114.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX113_Click(object sender, RoutedEventArgs e)
+        {
+            rec113.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO113_Click(object sender, RoutedEventArgs e)
+        {
+            rec113.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX112_Click(object sender, RoutedEventArgs e)
+        {
+            rec112.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO112_Click(object sender, RoutedEventArgs e)
+        {
+            rec112.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void btnX111_Click(object sender, RoutedEventArgs e)
+        {
+            rec111.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void btnO111_Click(object sender, RoutedEventArgs e)
+        {
+            rec111.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+    }
+}
