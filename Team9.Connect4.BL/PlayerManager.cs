98b84b53602437d298530971d169ad2e362b732c
Abigail Proudlock <300070180@fvtc.edu>
2023-04-21 17:53:03 -0500

    PLayerManager

--- Team9.Connect4.BL/PlayerManager.cs
+++ Team9.Connect4.BL/PlayerManager.cs
@@ -1,16 +1,110 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Team9.Connect4.BL.Models;
+using Team9.Connect4.PL;
 
 namespace Team9.Connect4.BL
 {
     public  class PlayerManager
     {
-        public static int Insert()
+        private const string RowError = "Row doesn't exist.";
+        public async static Task<int> Insert(Player player, bool rollback = false)
         {
+            try
+            {
+                IDbContextTransaction dbContextTransaction = null;
+                using (Connect4Entities dc = new Connect4Entities())
+                {
+                    if (rollback) dbContextTransaction = dc.Database.BeginTransaction();
 
+                    tblPlayer newrow = new tblPlayer();
+                    newrow.Id = Guid.NewGuid();
+                    newrow.Username = player.Username;
+                    newrow.Password = player.Password;
+                    newrow.SettingId = player.SettingId;
+
+                    dc.tblPlayers.Add(newrow);
+                    int results = dc.SaveChanges();
+
+                    if (rollback) dbContextTransaction.Rollback();
+                    return results;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static int Update(Player player, bool rollback = false)
+        {
+            try
+            {
+                int results = 0;
+                using (Connect4Entities dc = new Connect4Entities())
+                {
+                    IDbContextTransaction dbContextTransaction = null;
+                    if (rollback) dbContextTransaction = dc.Database.BeginTransaction();
+
+                    tblPlayer row = dc.tblPlayers.Where(p => p.Id == player.Id).FirstOrDefault();
+
+                    if (row != null)
+                    {
+                        row.Username = player.Username;
+                        row.Password = player.Password;
+                        row.SettingId = player.SettingId;
+
+                        results = dc.SaveChanges();
+
+                        if (rollback) dbContextTransaction.Rollback();
+                    }
+                }
+                return results;
+            }
+            catch (Exception)
+            {
+                throw new Exception(RowError);
+            }
+            
+        }
+
+        public static int Delete(Guid id, bool rollback = false)
+        {
+            try
+            {
+                int results = 0;
+                using (Connect4Entities dc = new Connect4Entities())
+                {
+                    IDbContextTransaction dbContextTransaction = null;
+                    if (rollback) dbContextTransaction = dc.Database.BeginTransaction();
+
+                    tblPlayer row = dc.tblPlayers.Where(p => p.Id == id).FirstOrDefault();
+
+                    if (row != null)
+                    {
+                        dc.tblPlayers.Remove(row);
+                        results = dc.SaveChanges();
+
+                        if (rollback) dbContextTransaction.Rollback();
+                    }
+                    else
+                    {
+                        throw new Exception(RowError);
+                    }
+                }
+                return results;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            
         }
     }
 }
