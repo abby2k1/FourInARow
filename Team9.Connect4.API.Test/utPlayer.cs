89b1947631a1a047a7eaf0745a6b2c44db556e26
Abigail Proudlock <300070180@fvtc.edu>
2023-04-28 18:54:33 -0500

    Added utBase, Api player controller and test

---
+++ Team9.Connect4.API.Test/utPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Team9.Connect4.BL.Models;
+using Team9.Connect4.API;
+
+namespace Team9.Connect4.API.Test
+{
+    [TestClass]
+    public class utPlayer : utBase
+    {
+        [TestMethod]
+        public async Task LoadTestAsync()
+        {
+            await base.LoadTestAsync<Player>();
+        }
+
+        [TestMethod]
+        public async Task InsertTestAsync()
+        {
+            Player player = new Player
+            {
+                Id = Guid.NewGuid(),
+                Username = "Test",
+                Password = "password",
+                SettingId = Guid.NewGuid()
+            };
+            await base.InsertTestAsync<Player>(player);
+        }
+
+        [TestMethod]
+        public async Task DeleteTestAsync()
+        {
+            await base.DeleteTestAsync<Player>(new KeyValuePair<string, string>("Username", "Test"));
+        }
+
+        [TestMethod]
+        public async Task UpdateTestAsync()
+        {
+            Player player = new Player
+            {
+                Id = Guid.NewGuid(),
+                Username = "UpdateTest",
+                Password = "password",
+                SettingId = Guid.NewGuid()
+            };
+            await base.UpdateTestAsync<Player>(new KeyValuePair<string, string>("Password", "UpdatedPassword"), player);
+        }
+
+    }
+}
--- Team9.Connect4.API/Controllers/PlayerController.cs
+++ Team9.Connect4.API/Controllers/PlayerController.cs
@@ -1,6 +1,71 @@
-﻿namespace Team9.Connect4.API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Team9.Connect4.BL;
+
+namespace Team9.Connect4.API.Controllers
 {
-    public class PlayerController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
     {
+        // GET: api/<PlayerController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BL.Models.Player>>> Get()
+        {
+            try
+            {
+                return Ok(await PlayerManager.Load());
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        // POST api/<PlayerController>
+        [HttpPost("{rollback}")]
+        public async Task<ActionResult> Post([FromBody] BL.Models.Player player, bool rollback = false)
+        {
+            try
+            {
+                await PlayerManager.Insert(player, rollback);
+                return Ok(player.Id);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        // PUT api/<PlayerController>/5
+        [HttpPut("{id}/{rollback?}")]
+        public async Task<IActionResult> Put(Guid id, [FromBody] BL.Models.Player player, bool rollback = false)
+        {
+            try
+            {
+                return Ok(await PlayerManager.Update(player, rollback));
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        // DELETE api/<PlayerController>/5
+        [HttpDelete("{id}/{rollback?}")]
+        public async Task<IActionResult> Delete(Guid id, bool rollback = false)
+        {
+            try
+            {
+                return Ok(await PlayerManager.Delete(id, rollback));
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
     }
 }
