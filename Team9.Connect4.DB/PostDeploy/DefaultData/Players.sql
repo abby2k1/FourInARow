2162dfd6f37a3037470ec8b9b74b47dff3dce95f
Abigail Thompson <abigail.thompson2001@outlook.com>
2023-04-28 20:32:08 -0500

    added # to hex colors

--- Team9.Connect4.DB/PostDeploy/DefaultData/Players.sql
+++ Team9.Connect4.DB/PostDeploy/DefaultData/Players.sql
@@ -1,18 +1,18 @@
 ﻿BEGIN
 	DECLARE @SettingsId uniqueidentifier;
-	SELECT @SettingsId = Id from tblSetting where PlayerColor = '00FF00' 
+	SELECT @SettingsId = Id from tblSetting where PlayerColor = '#00FF00' 
 
 	INSERT INTO dbo.tblPlayer (Id, Username, Password, SettingId)
 	VALUES
 	(NEWID(), 'MaxWilke', 'Maple', @SettingsId)
 
-	SELECT @SettingsId = Id from tblSetting where PlayerColor = 'FF0000' 
+	SELECT @SettingsId = Id from tblSetting where PlayerColor = '#FF0000' 
 
 	INSERT INTO dbo.tblPlayer (Id, Username, Password, SettingId)
 	VALUES
 	(NEWID(), 'AbbyThompson', 'Maple', @SettingsId)
 
-	SELECT @SettingsId = Id from tblSetting where PlayerColor = '0000FF' 
+	SELECT @SettingsId = Id from tblSetting where PlayerColor = '#0000FF' 
 
 	INSERT INTO dbo.tblPlayer (Id, Username, Password, SettingId)
 	VALUES
--- Team9.Connect4.DB/PostDeploy/DefaultData/Settings.sql
+++ Team9.Connect4.DB/PostDeploy/DefaultData/Settings.sql
@@ -1,7 +1,7 @@
 ﻿BEGIN
 	INSERT INTO dbo.tblSetting (Id, PlayerColor, OpponentColor, BoardColor)
 	VALUES
-	(NEWID(), '0000FF', 'FF0000', '000000'),
-	(NEWID(), '00FF00', 'FFFF00', 'FFFFFF'),
-	(NEWID(), 'FF00FF', '00FF00', '666666')
+	(NEWID(), '#0000FF', '#FF0000', '#000000'),
+	(NEWID(), '#00FF00', '#FFFF00', '#FFFFFF'),
+	(NEWID(), '#FF00FF', '#00FF00', '#666666')
 END
