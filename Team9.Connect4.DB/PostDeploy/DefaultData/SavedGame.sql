81b166868a100b0cb41b8a2873c28ef0de2e9090
Max <maxwell.wilke@yahoo.com>
2023-05-07 20:49:50 -0500

    Added new values into savedgame.sql

--- Team9.Connect4.DB/PostDeploy/DefaultData/SavedGame.sql
+++ Team9.Connect4.DB/PostDeploy/DefaultData/SavedGame.sql
@@ -1,20 +1,29 @@
 ﻿BEGIN 
 	DECLARE @ResultsId uniqueidentifier;
-	SELECT @PlayerId = Id from tblPlayer where Username = 'MaxWilke' 
+	DECLARE @Player1Id uniqueidentifier;
+	DECLARE @Player2Id uniqueidentifier;
 
-	INSERT INTO DBO.tbtblSavedGame(Id, ResultsId, Player1ID, Player2ID, BoardState)
+	SELECT @ResultsId = Id from tblResult where Turns = 10 
+	SELECT @Player1Id = Id from tblPlayer where Username = 'AbbyThompson'
+	SELECT @Player2Id = Id from tblPlayer where Username = 'AbbieProudlock'
+
+	INSERT INTO DBO.tblSavedGame(Id, ResultsId, Player1Id, Player2Id, BoardState)
 	VALUES
-	(NEWID(), @PlayerId, @PlayerId, 12)
+	(NEWID(), @ResultsId, @Player1Id, @Player2Id, '00000000000000000000000000000000000000121200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000')
 	
-	SELECT @PlayerId = Id from tblPlayer where Username = 'AbbyThompson' 
+	SELECT @ResultsId = Id from tblResult where Turns = 12 
+	SELECT @Player1Id = Id from tblPlayer where Username = 'AbbyThompson'
+	SELECT @Player2Id = Id from tblPlayer where Username = 'MaxWilke'
 
-	INSERT INTO DBO.tblResult (Id, WinnerId, LoserID, Turns)
+	INSERT INTO DBO.tblSavedGame(Id, ResultsId, Player1Id, Player2Id, BoardState)
 	VALUES
-	(NEWID(), @PlayerId, @PlayerId, 14) 
+	(NEWID(), @ResultsId, @Player1Id, @Player2Id, '00000000000000000000000000000000000000121200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000')
 
-	SELECT @PlayerId = Id from tblPlayer where Username = 'AbbieProudlock' 
+	SELECT @ResultsId = Id from tblResult where Turns = 14 
+	SELECT @Player1Id = Id from tblPlayer where Username = 'MaxWilke'
+	SELECT @Player2Id = Id from tblPlayer where Username = 'AbbieProudlock'
 
-	INSERT INTO DBO.tblResult (Id, WinnerId, LoserID, Turns)
+	INSERT INTO DBO.tblSavedGame(Id, ResultsId, Player1Id, Player2Id, BoardState)
 	VALUES
-	(NEWID(), @PlayerId, @PlayerId, 10) 
+	(NEWID(), @ResultsId, @Player1Id, @Player2Id, '00000000000000000000000000000000000000121200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000')
 END
