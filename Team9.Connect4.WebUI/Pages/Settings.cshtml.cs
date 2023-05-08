4e906430a99264a7a6916a8999b8d35cb9032474
Abigail Proudlock <300070180@fvtc.edu>
2023-05-08 16:21:48 -0500

    Added a Settings page

---
+++ Team9.Connect4.WebUI/Pages/Settings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Team9.Connect4.WebUI.Pages
+{
+    public class SettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
