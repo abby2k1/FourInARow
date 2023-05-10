7a937b50d5c449420083f9d231da011dca848011
Abigail Proudlock <300070180@fvtc.edu>
2023-05-09 22:40:52 -0500

    Finished all ASP UIs

---
+++ Team9.Connect4.WebUI/Pages/Leaderboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Team9.Connect4.WebUI.Pages.Shared
+{
+    public class LeaderboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
