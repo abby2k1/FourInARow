ba4cc748ed075d0ab31322187972475fe966c5a5
Abigail Proudlock <300070180@fvtc.edu>
2023-05-10 22:48:21 -0500

    Leaderboard hooked up to stored procedure

--- Team9.Connect4.WebUI/Pages/Leaderboard.cshtml.cs
+++ Team9.Connect4.WebUI/Pages/Leaderboard.cshtml.cs
@@ -1,10 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using Team9.Connect4.BL.Models;
+using System.Data;
 
 namespace Team9.Connect4.WebUI.Pages.Shared
 {
     public class LeaderboardModel : PageModel
     {
+        public List<Leaderboard> GetLeaderboard()
+        {
+            List<Leaderboard> leaderboards = new List<Leaderboard>();
+
+            using (SqlConnection conn = new SqlConnection("Server=(localhost)\\MSSQLLocalDB;Database=Team9.Connect4.DB;Integrated Security=true"))
+            {
+                conn.Open();
+
+                SqlCommand sqlCommand = new SqlCommand("spLeaderboard", conn);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                SqlDataReader dataReader = sqlCommand.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Leaderboard leaderboard = new Leaderboard();
+                    leaderboard.Winner = dataReader.GetString(0);
+                    leaderboard.Turns = dataReader.GetInt32(1);
+
+                    leaderboards.Add(leaderboard);
+                }
+            }
+            return leaderboards;
+        }
+
         public void OnGet()
         {
         }
