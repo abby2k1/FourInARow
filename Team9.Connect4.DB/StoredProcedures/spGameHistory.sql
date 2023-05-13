c9cb63e43f862b97f45d2a00bd5afd768bd4237b
Abigail Proudlock <300070180@fvtc.edu>
2023-05-13 16:05:38 -0500

    Added to GameHistory page and added new sp

---
+++ Team9.Connect4.DB/StoredProcedures/spGameHistory.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[spGameHistory]
+AS
+	
+
+RETURN
--- Team9.Connect4.WebUI/Pages/GameHistory.cshtml.cs
+++ Team9.Connect4.WebUI/Pages/GameHistory.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Team9.Connect4.WebUI.Pages
+namespace Team9.Connect4.WebUI.Pages.Shared
 {
     public class GameHistoryModel : PageModel
     {
