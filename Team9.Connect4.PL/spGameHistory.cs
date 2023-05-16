f1add7943705b7c1777a3c3ec0cff9d13a12d843
Abigail Proudlock <300070180@fvtc.edu>
2023-05-15 21:24:16 -0500

    Removed unnecessary files

--- Team9.Connect4.PL/spGameHistory.cs
+++ Team9.Connect4.PL/spGameHistory.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Team9.Connect4.BL.Models
+namespace Team9.Connect4.PL
 {
     public class GameHistory
     {
         public Guid Results { get; set; }
         public string Player1Username { get; set; }
-        public string Player2Username { get; set;}
-        public string BoardState { get; set;}
-        public string GameCode { get; set;}
-        public int Turns { get; set;}
+        public string Player2Username { get; set; }
+        public string BoardState { get; set; }
+        public string GameCode { get; set; }
+        public int Turns { get; set; }
     }
 }
--- Team9.Connect4.PL/spLeaderboard.cs
+++ Team9.Connect4.PL/spLeaderboard.cs
@@ -8,9 +8,8 @@ namespace Team9.Connect4.PL
 {
     class spLeaderboard
     {
-        public Guid ID { get; set; }
-        public Guid SettingsID { get; set; }
-        public int Turns { get; set; }
+        //public Guid ID { get; set; }
         public string Username { get; set; }
+        public int Turns { get; set; }
     }
 }
