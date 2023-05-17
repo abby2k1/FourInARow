5f66c1200189337aa9f17bafea3568d3b5375e5f
Abigail Proudlock <300070180@fvtc.edu>
2023-05-16 21:26:12 -0500

    Created Login page

---
+++ Team9.Connect4.WebUI/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Team9.Connect4.WebUI.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
